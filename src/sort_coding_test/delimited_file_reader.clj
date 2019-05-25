212d44bf3ca3d78df65679fa44bbb2f2fc0fa81a
Mark Herman, II <mark@exceptionaltech.com>
2019-05-24 19:08:09 -0500

    File reading and tests for it

--- src/sort_coding_test/delimited_file_reader.clj
+++ src/sort_coding_test/delimited_file_reader.clj
@@ -1,20 +1,32 @@
 (ns sort-coding-test.delimited-file-reader
-  (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
+            [clojure.string :as string]))
 
 (def delimiters
-  "Maps delimiters from their names to their strings."
-  {"pipe" " | "
-   "comma" ", "
-   "space" " "})
+  "Maps delimiters from their names to their characters."
+  {"pipe" \|
+   "comma" \,
+   "space" \space})
 
+(def valid-delimiter-names
+  (keys delimiters))
 
+;; NOTE: Since we're sorting in memory, all of the entries must be read at some point.
+;; So, this returns all of the lines rather than a lazy seq so the file handle
+;; can be let go.
+(defn read-all-lines
+  "Reads the lines of the file with the given filename using the
+  provided delimiter. Output looks as expected by the Clojure CSV library
+  but with the strings trimmed."
+  [filename delimiter-name]
+  (when-not (.exists (io/file filename))
+    (throw (java.io.FileNotFoundException. (str "The file " filename " was not found."))))
 
-;; (with-open [reader (io/reader "in-file.csv")]
-;;   (doall
-;;     (csv/read-csv reader)))
-
-;; (with-open [writer (io/writer "out-file.csv")]
-;;   (csv/write-csv writer
-;;                  [["abc" "def"]
-;;                   ["ghi" "jkl"]]))
+  (if-let [delimiter (get delimiters delimiter-name)]
+    (with-open [reader (io/reader filename)]
+      (let [file-lines (csv/read-csv reader :separator delimiter)]
+        (doall (for [file-line file-lines]
+                 (map string/trim file-line)))))
+    (throw (AssertionError. (str "Unsupported delimiter name passed. "
+                                 "Valid delimiter names are: " valid-delimiter-names)))))
--- test/sort_coding_test/delimited_file_reader_test.clj
+++ test/sort_coding_test/delimited_file_reader_test.clj
@@ -5,17 +5,29 @@
             [clojure.edn :as edn]
             [sort-coding-test.test-data :as test-data]))
 
+(def test-file-generator-delimiters
+  "The test files have whitespace around some delimiters. I want to make sure I'm
+  testing with the same file format this will be tested with. I'll use Clojure's CSV
+  reader and trim the data when reading."
+  {"pipe" " | "
+   "comma" ", "
+   "space" " "})
+
+(defn test-filename
+  [delimiter-name]
+  (str "temp/test-" delimiter-name "-delimited-people.csv"))
+
 ;; I originally intended to use the CSV writer for this, but it only supports
 ;; character and not string separators.
 (defn create-test-files
   "Adds people into test delimited files. The filename will be in the form
   'temp/test-delimiter_name-delimited-people.csv"
   []
-  (doseq [delimiter delimiters]
-    (let [[name separator] delimiter]
-      (with-open [w (io/writer (str "temp/test-" name "-delimited-people.csv"))]
+  (doseq [delimiter test-file-generator-delimiters]
+    (let [[delimiter-name delimiter-separator] delimiter]
+      (with-open [w (io/writer (test-filename delimiter-name))]
         (doseq [test-person test-data/unordered-people-strings]
-          (.write w (str (clojure.string/join separator test-person) "\n")))))))
+          (.write w (str (clojure.string/join delimiter-separator test-person) "\n")))))))
 
 (defn gen-test-files-fixture
   [f]
@@ -24,5 +36,18 @@
 
 (use-fixtures :once gen-test-files-fixture)
 
-(testing "Read delimited file"
-  (is (= 0 0)))
+(deftest read-delimited-files
+  (testing "Reading data from files with all required delimiters"
+    ; It should be the same as the data that generated them
+    (doseq [delimiter delimiters]
+      (let [[delimiter-name delimiter-separator] delimiter]
+        (is (= test-data/unordered-people-strings
+               (read-all-lines (test-filename delimiter-name) delimiter-name))))))
+
+  (testing "Exception handling"
+    (let [valid-delimiter-name (key (first delimiters))
+          filename-that-exists (test-filename valid-delimiter-name)]
+      (is (thrown? java.io.FileNotFoundException
+                   (read-all-lines "file-that-does-not-exist" valid-delimiter-name)))
+      (is (thrown? AssertionError
+                   (read-all-lines filename-that-exists "invalid-delimiter-name"))))))
