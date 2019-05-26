bf02d7e8d7c06b075ca58a2ad03906f9c4bfac7d
Mark Herman, II <mark@exceptionaltech.com>
2019-05-26 07:30:40 -0500

    More testing on command-line options parsing

--- test/sort_coding_test/core_test.clj
+++ test/sort_coding_test/core_test.clj
@@ -1,7 +1,8 @@
 (ns sort-coding-test.core-test
   (:require [clojure.test :refer :all]
             [sort-coding-test.core :refer :all]
-            [clojure.tools.cli :refer [parse-opts]]))
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.java.io :as io]))
 
 (def file-options
   {:comma-delimited-file "-c"
@@ -22,4 +23,9 @@
                         [option-string "test1.csv" option-string "test2.csv"] (cli-options))
                        [:options option-key]))
             (str "accumulates file arguments for " option-string)))
-      (is (= true (get-in (parse-opts ["--start-server"] (cli-options)) [:options :start-server]))))))
+      (is (get-in (parse-opts ["--start-server"] (cli-options)) [:options :start-server]))
+      (is (not (get-in (parse-opts [] (cli-options)) [:options :start-server])))))
+
+  (testing "Detects file existence errors"
+    (is (empty? (:errors (parse-opts ["-c" (io/resource "empty_file.csv")] (cli-options)))))
+    (is (= 1 (count (:errors (parse-opts ["-c" "does_not_exist.csv"] (cli-options))))))))
